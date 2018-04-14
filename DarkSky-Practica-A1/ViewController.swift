24116de1bf21e7c1388da03fb359be075d39d38d
George Royce <george.royce@detroitlabs.com>
2018-04-14 14:36:09 -0400

    Initial Commit

---
+++ DarkSky-Practica-A1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DarkSky-Practica-A1
+//
+//  Created by George Royce on 4/14/18.
+//  Copyright © 2018 George Royce. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
